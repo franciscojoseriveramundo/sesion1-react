951cb780e7d724135f631ffc6347f39206d76b55
Francisco <francisco.rivera@servitron.net>
2022-03-29 23:34:01 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import FilaFocos from './Ejemplo 2/FilaFocos';
+import FocoMercurial from "./Ejemplo 2/FocoMercurial";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="techo">
+       <FocoMercurial />
+       <FilaFocos />
+       <FocoMercurial />
     </div>
-  );
+ );
+
 }
 
 export default App;
